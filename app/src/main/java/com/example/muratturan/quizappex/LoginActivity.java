98300ecc42f3263e3c0082d34d217500a7a9e219
AMuratTuran <0muratturan@gmail.com>
2017-08-15 02:09:43 +0300

    Updated

--- app/src/main/java/com/example/muratturan/quizappex/LoginActivity.java
+++ app/src/main/java/com/example/muratturan/quizappex/LoginActivity.java
@@ -1,23 +1,18 @@
 package com.example.muratturan.quizappex;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,7 +22,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.Map;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
 
     private Button loginButton;
@@ -39,43 +34,36 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseDatabase database;
     private DatabaseReference myRef;
     private ArrayList<String> usernames;
-
+    private ArrayList<String> highscores = new ArrayList<>();
+    private ArrayList<String> names ;
+    private ArrayList<String> lastnames ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        loginButton = (Button) findViewById(R.id.loginButton);
-        createUser = (Button) findViewById(R.id.createAccountButton);
-        userNameEdit = (EditText) findViewById(R.id.usernameEditText);
+        loginButton = findViewById(R.id.loginButton);
+        createUser = findViewById(R.id.createAccountButton);
+        userNameEdit = findViewById(R.id.usernameEditText);
 
 
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference("users");
 
 
-        mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-
-                if (user != null && user.isEmailVerified()) {
-                    startActivity(new Intent(LoginActivity.this, StartActivity.class));
-                    Log.d("******", "onAuthStateChanged:signed_in:" + user.getUid());
-
-                } else {
-
-                    Log.d("-------", "onAuthStateChanged:signed_out");
-                }
-            }
-        };
-
         myRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                usernames = collectTitles((Map<String,Object>) dataSnapshot.getValue());
+                usernames = collectTitles((Map<String, Object>) dataSnapshot.getValue(),"username");
+                names = collectTitles((Map<String, Object>) dataSnapshot.getValue(),"name");
+                lastnames = collectTitles((Map<String, Object>) dataSnapshot.getValue(),"lastname");
+
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+
+                    System.out.println(ds.child("highScore"));
+                }
+
 
             }
 
@@ -84,6 +72,15 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                Log.d("-------", "onAuthStateChanged:signed_out");
+            }
+        };
+
 
         createUser.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -133,37 +130,25 @@ public class LoginActivity extends AppCompatActivity {
             progressDialog.setMessage("Logging in...");
             progressDialog.show();
 
-
-            if(usernames.contains(username)){
+            if (usernames.contains(username)) {
                 progressDialog.hide();
-                startActivity(new Intent(LoginActivity.this,StartActivity.class));
-            }
-            else
+                Intent mIntent = new Intent(LoginActivity.this, StartActivity.class);
+                startActivity(mIntent);
+            } else
                 Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show();
-
-
-
-
         }
     }
 
-    private ArrayList<String> collectTitles(Map<String, Object> posts) {
+    private ArrayList<String> collectTitles(Map<String, Object> posts,String tag) {
 
-        ArrayList<String> titles = new ArrayList<>();
-        //iterate through each user, ignoring their UID
+        ArrayList<String> info = new ArrayList<>();
 
-        for (Map.Entry<String, Object> entry : posts.entrySet()){
-
+        for (Map.Entry<String, Object> entry : posts.entrySet()) {
 
             Map singlePost = (Map) entry.getValue();
-
-            titles.add((String) singlePost.get("username"));
+            info.add((String) singlePost.get(tag));
         }
 
-
-        return titles;
-
+        return info;
     }
-
-
 }
--- app/src/main/java/com/example/muratturan/quizappex/StartActivity.java
+++ app/src/main/java/com/example/muratturan/quizappex/StartActivity.java
@@ -1,39 +1,212 @@
 package com.example.muratturan.quizappex;
 
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
+import android.support.v4.view.ActionProvider;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class StartActivity extends AppCompatActivity {
 
-    private Button startButton;
-    private String userName;
+
+public class StartActivity extends ActionBarActivity {
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private ActionProvider shareActionProvider;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private int currentPosition = 0;
+
+    private String[] titles;
+    private ListView drawerList;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        titles = getResources().getStringArray(R.array.titles);
+        drawerList = (ListView) findViewById(R.id.drawer);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        Intent mIntent = getIntent();
-        userName = mIntent.getStringExtra("userNameData");
+        drawerList.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_activated_1, titles));
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
 
-        startButton = (Button) findViewById(R.id.start_button);
-        startButton.setOnClickListener(new View.OnClickListener() {
+
+
+        if (savedInstanceState != null) {
+            currentPosition = savedInstanceState.getInt("position");
+            setActionBarTitle(currentPosition);
+        } else {
+            selectItem(0);
+        }
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+                R.string.open_drawer, R.string.close_drawer) {
+            //Called when a drawer has settled in a completely closed state
             @Override
-            public void onClick(View v) {
-                useIntent();
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu();
             }
-        });
+
+            //Called when a drawer has settled in a completely open state.
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+        drawerLayout.setDrawerListener(drawerToggle);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        getFragmentManager().addOnBackStackChangedListener(
+                new FragmentManager.OnBackStackChangedListener() {
+                    public void onBackStackChanged() {
+                        FragmentManager fragMan = getFragmentManager();
+                        Fragment fragment = fragMan.findFragmentByTag("visible_fragment");
+                        if (fragment instanceof TopPlayersFragment) {
+                            currentPosition = 0;
+                        }
+                        if (fragment instanceof PointFragment) {
+                            currentPosition = 1;
+                        }
+                        if (fragment instanceof FriendsListFragment) {
+                            currentPosition = 2;
+                        }
+                        if(fragment instanceof ProfileFragment){
+                            currentPosition = 3 ;
+                        }
+                        setActionBarTitle(currentPosition);
+                        drawerList.setItemChecked(currentPosition, true);
+                    }
+                }
+        );
+
+    }
+
+    private void selectItem(int position) {
+        // update the main content by replacing fragments
+        currentPosition = position;
+        Fragment fragment;
+        switch (position) {
+            case 1:
+                fragment = new PointFragment();
+                break;
+            case 3:
+                fragment = new ProfileFragment();
+                break;
+            case 2:
+                fragment = new FriendsListFragment();
+                break;
+            default:
+                fragment = new TopPlayersFragment();
+                break;
+        }
+
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame, fragment, "visible_fragment");
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
+
+        setActionBarTitle(position);
+        drawerLayout.closeDrawer(drawerList);
+    }
+
+    private void setActionBarTitle(int position) {
+        String title;
+        if (position == 0) {
+            title = getResources().getString(R.string.app_name);
+        } else {
+            title = titles[position];
+        }
+
+
+        getSupportActionBar().setTitle(title);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
+        menu.findItem(R.id.action_share).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("position", currentPosition);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider =  MenuItemCompat.getActionProvider(menuItem);
+        setIntent("This is example text");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void setIntent(String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        switch (item.getItemId()) {
+
+            case R.id.action_search:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 
-
-    private void useIntent() {
-        Intent intent = new Intent(this, CategoryActivity.class);
-        intent.putExtra("userNameData", userName);
-        startActivity(intent);
-    }
 }
